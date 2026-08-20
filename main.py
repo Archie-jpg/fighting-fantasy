0310922309d451e1942ac7eda14247f407a284e2
Archie-jpg <archieleppard@gmail.com>
2026-08-20 11:59:25 +0100

    Directed main window straigh to home screen

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QStackedWidget
+from windows.home import HomeWindow
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
-        self.button = QPushButton("My Button", self)
-        self.button.clicked.connect(self.handleButton)
+        self.screens = QStackedWidget(parent=self)
+        # Add Home Screen
+        self.home_screen = HomeWindow()
+        self.screens.addWidget(self.home_screen)
 
-    def handleButton(self):
-        self.button.setText("Ready")
+        self.screens.setCurrentWidget(self.home_screen)
 
 def main():
     app = QApplication([])
--- windows/home.py
+++ windows/home.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel
 
 class HomeWindow(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
+        self.title = QLabel("Home", parent=self)
