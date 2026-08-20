7c3eca4de1ed433636de796a649e1dccdf2ac891
Archie-jpg <archieleppard@gmail.com>
2026-08-20 11:50:27 +0100

    Started on the gui

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.button = QPushButton("My Button", self)
+        self.button.clicked.connect(self.handleButton)
+
+    def handleButton(self):
+        self.button.setText("Ready")
+
+def main():
+    app = QApplication([])
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
