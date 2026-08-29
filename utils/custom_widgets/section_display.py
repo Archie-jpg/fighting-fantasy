f4d4955c6a604bdd7e1095ebae6070f425e230b3
Archie-jpg <archieleppard@gmail.com>
2026-08-29 12:58:03 +0100

    Fixed a bug with options not getting cleared when moving between them

--- utils/custom_widgets/section_display.py
+++ utils/custom_widgets/section_display.py
@@ -5,13 +5,28 @@ from classes.adventure_reader import AdventureReader, Section, Option
 from utils.custom_widgets.option_button import QOptionButton
 
 class OptionsContainter(QWidget):
+    option_chosen: Signal = Signal(str)
+    
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
         
-    def load_options(self, options: list[str]):
-        pass
+    @Slot(str)
+    def choose_option(self, next_section: str):
+        self.option_chosen.emit(next_section)
+        
+    def load_options(self, options: list[Option]):
+        for option in options:
+            btn_option = QOptionButton(f"{option.text}", option.next_section)
+            btn_option.clicked.connect(self.choose_option)
+            self.main_layout.addWidget(btn_option)
+    
+    def clear(self):
+        while self.main_layout.count() > 0:
+            item = self.main_layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None: widget.deleteLater()
 
 
 class SectionDisplay(QWidget):
@@ -31,16 +46,15 @@ class SectionDisplay(QWidget):
         self.lay_main.addWidget(self.lbl_section_text)
         self.lay_main.addStretch()
         self.options_container = OptionsContainter()
+        self.options_container.option_chosen.connect(self.load_next_section)
         self.lay_main.addWidget(self.options_container, alignment=Qt.AlignmentFlag.AlignBottom)
         self.setLayout(self.lay_main)
         
     def display_section(self, section: Section):
+        self.options_container.clear()
         self.lbl_section_number.setText(section.number)
         self.lbl_section_text.setText(section.description)
-        for option in section.options:
-            btn_option = QOptionButton(f"{option.text}", option.next_section)
-            btn_option.clicked.connect(self.load_next_section)
-            self.options_container.main_layout.addWidget(btn_option)
+        self.options_container.load_options(section.options)
         
     def load_adventure(self, adventure_file: str, section: int = 0):
         self.adventure = AdventureReader(adventure_file)
